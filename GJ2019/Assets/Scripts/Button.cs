0f495015d2c7446ba4fa4e70182053d0384b2601
Max <max.bauer7590@gmail.com>
2019-09-14 14:43:27 -0700

    Added walls and buttons

---
+++ GJ2019/Assets/Scripts/Button.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : LevelObject
+{
+
+    public int weightRequired;
+    public bool weightSatisfied;
+    public float sinkDistance;
+    public float compressionSpeed;
+
+    private float weightOnButton;
+    private float startingPositionY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject go = this.transform.parent.GetComponent<LevelObject>().gameObject;
+        startingPositionY = go.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject go = this.transform.parent.GetComponent<LevelObject>().gameObject;
+        Vector3 currentPosition = go.transform.position;
+
+        if (weightSatisfied && go.transform.position.y > startingPositionY - sinkDistance)
+        {
+            go.transform.position = new Vector3(currentPosition.x, currentPosition.y - compressionSpeed * Time.deltaTime, currentPosition.z);
+        }
+        else if (!weightSatisfied)
+        {
+            if (go.transform.position.y < startingPositionY)
+            {
+                go.transform.position = new Vector3(currentPosition.x, currentPosition.y + compressionSpeed * Time.deltaTime, currentPosition.z);
+            }
+            if (go.transform.position.y > startingPositionY)
+            {
+                go.transform.position = new Vector3(currentPosition.x, startingPositionY, currentPosition.z);
+            }
+        }
+
+
+    }
+
+    void OnCollisionEnter(Collider other)
+    {
+        weightOnButton += other.attachedRigidbody.mass;
+        if (weightOnButton >= weightRequired)
+        {
+            weightSatisfied = true;
+        }
+
+    }
+
+    void OnCollisionExit(Collider other)
+    {
+        weightOnButton -= other.attachedRigidbody.mass;
+        if (weightOnButton < weightRequired)
+        {
+            weightSatisfied = false;
+        }
+    }
+}
+
+
