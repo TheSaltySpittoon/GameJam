57cb74040b5eb871d085c5a319abde9f75438468
Bijan Beglari <BijanBeglari@gmail.com>
2019-09-14 11:08:35 -0700

    Project Setup

---
+++ GJ2019/Assets/Scripts/LevelObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelObject : MonoBehaviour
+{
+    public bool HasCollision = true;
+    public bool Interactable = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
