b278d645b5d3a159814a433f7cca6485f5164c63
Max <max.bauer7590@gmail.com>
2019-09-15 21:06:47 -0700

    Level Ender implemented

---
+++ GJ2019/Assets/Scripts/LevelEnder.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class LevelEnder : LevelObject
+{
+
+    private bool ricIsHere = false;
+    private bool bolIsHere = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ricIsHere && bolIsHere)
+        {
+            //SceneManager.LoadScene(nextScene);
+            string currScene = SceneManager.GetActiveScene().name;
+            if (currScene == "SoloTutorial")
+            {
+                SceneManager.LoadScene("BallTutorial");
+            }
+            else if (currScene == "BallTutorial")
+            {
+                SceneManager.LoadScene("Level3");
+            }
+            else
+            {
+                SceneManager.LoadScene("SoloTutorial");
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "PlayerRaccoon")
+        {
+            ricIsHere = true;
+        }
+        else if (other.gameObject.name == "PlayerBall")
+        {
+            bolIsHere = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "PlayerRaccoon")
+        {
+            ricIsHere = false;
+        }
+        else if (other.gameObject.name == "PlayerBall")
+        {
+            bolIsHere = false;
+        }
+    }
+}
